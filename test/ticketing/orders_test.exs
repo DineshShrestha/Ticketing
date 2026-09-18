58024e7d29a44685e19b85b3c2080fa9f59d5c96
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2026-09-18 14:12:15 +0200

    Add cancel_order tests including double-cancel guard

--- test/ticketing/orders_test.exs
+++ test/ticketing/orders_test.exs
@@ -66,4 +66,31 @@ defmodule Ticketing.OrdersTest do
     assert count == 5
     assert updated_ticket_type.quantity == 0
   end
+
+  test "cancel_order restore inventory and marks order cancelled", %{
+    ticket_type: ticket_type,
+    user: user
+  } do
+    assert {:ok, %{order: order}} = Orders.purchase_tickets(user, ticket_type.id, 2)
+    assert {:ok, _} = Orders.cancel_order(order)
+
+    cancelled_order = Orders.get_order!(order.id)
+    assert cancelled_order.status == "cancelled"
+    updated_ticket_type = TicketTypes.get_ticket_type!(ticket_type.id)
+    assert updated_ticket_type.quantity == 5
+  end
+
+  test "double_cancel guard", %{ticket_type: ticket_type, user: user} do
+    assert {:ok, %{order: order}} = Orders.purchase_tickets(user, ticket_type.id, 2)
+
+    assert {:ok, _} = Orders.cancel_order(order)
+    cancelled_order = Orders.get_order!(order.id)
+    assert cancelled_order.status == "cancelled"
+    updated_ticket_type = TicketTypes.get_ticket_type!(ticket_type.id)
+    assert updated_ticket_type.quantity == 5
+
+    assert {:error, :cancel, :order_not_cancellable, %{}} = Orders.cancel_order(order)
+    still_updated_ticket_type = TicketTypes.get_ticket_type!(ticket_type.id)
+    assert still_updated_ticket_type.quantity == 5
+  end
 end
